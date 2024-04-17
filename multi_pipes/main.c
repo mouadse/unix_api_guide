44b8d95f477765f88daad6bdce07cb2211a04874
mouadse <mouadse@gmail.com>
2024-04-17 22:27:20 +0100

    x

--- multi_pipes/main.c
+++ multi_pipes/main.c
@@ -6,19 +6,19 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  int pid;
-  int status;
-  char *args[] = {"/bin/ls", "-l", NULL};
+	int pid;
+	int status;
+	char *args[] = {"/bin/ls", "-l", NULL};
 
-  pid = fork();
-  if (pid == 0) {
-    execv(args[0], args);
-    perror("execv");
-    exit(1);
-  } else {
-    wait(&status);
-    printf("Child process terminated with status %d\n", status);
-  }
+	pid = fork();
+	if (pid == 0) {
+		execv(args[0], args);
+		perror("execv");
+		exit(1);
+	} else {
+		wait(&status);
+		printf("Child process terminated with status %d\n", status);
+	}
 
-  return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
