d1e8578b481a3d6380efacc80d62f5f3561fb8cf
mouadse <mouadse@gmail.com>
2024-04-18 02:58:14 +0100

    x

--- queues/main.c
+++ queues/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 02:57:32 by msennane          #+#    #+#             */
-/*   Updated: 2024/04/18 02:57:32 by msennane         ###   ########.fr       */
+/*   Created: 2024/04/18 02:57:55 by msennane          #+#    #+#             */
+/*   Updated: 2024/04/18 02:57:56 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int	main(void)
 {
-	// code it here
 	printf("Hello, World!\n");
 	return (EXIT_SUCCESS);
 }
